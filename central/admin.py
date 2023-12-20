c6c20d6c0d94452fb8e9976cfb65770d2ff78401
nfl6fh <89548640+nfl6fh@users.noreply.github.com>
2023-12-20 11:44:57 -0500

    register models to admin

--- central/admin.py
+++ central/admin.py
@@ -3,3 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Shell)
+admin.site.register(Lineup)
+admin.site.register(Extra_Workout)
+admin.site.register(Absence)
